cd09e860ed029a5d94910476ce9b07d7c1645c23
Saibo-creator <saibo.geng@yahoo.fr>
2021-04-01 12:20:26 +0200

    [feat] add multi process but not enabled yet due to number of cores available

--- run_download_and_label.py
+++ run_download_and_label.py
@@ -2,13 +2,29 @@ import copy
 import shutil
 import os
 import time
-from image_downloader import google_download
+
 import csv
 import logging
 from ImageLabelingPackage.ImageDownloadLabeler import ImageDownloader
 from deepface import DeepFace
 from PyS3Upload.PyS3Uploader import S3Uploader
 from PyS3Upload.helper import get_time_identifier
+from image_downloader import google_download
+from concurrent.futures import ProcessPoolExecutor, as_completed
+
+def verify(reference_img_path, img_path):
+    try:
+        result = DeepFace.verify(reference_img_path, img_path, enforce_detection=False,
+                                 model_name='VGG-Face')
+    except AttributeError as e:  # case when input image is a pdf, then the package can not read it.
+        log.info(e)
+        result = {"verified": False}
+
+    if not result["verified"]:
+        os.remove(img_path)
+        return 0
+    else:
+        return 1
 
 if __name__ == '__main__':
 
@@ -18,14 +34,16 @@ if __name__ == '__main__':
     IMAGE_RECALL_NUM = 50
     REMOVE_REFERENCE_IMG = False
     REMOVE_LOCAL_AFTER_UPLOAD = True
+    MULTIPROCESS = False
+    NUM_WORKERS = 5
 
     startTime = int(round(time.time()))
 
     line_list = []
-    query_fn = 'query_2000_2020_6147.csv'
+    query_fn = 'query_results/has_img/query_2000_2020_6147_rest.csv'
     # runtime_query_fn = f'{query_fn.split(".")[0]}_runtime.csv'
 
-    with open('query_results/has_img/query_2000_2020_6147_rest.csv', newline='') as csvfile:
+    with open(query_fn, newline='') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=',')
         for row in spamreader:
             line_list.append(row)
@@ -50,19 +68,21 @@ if __name__ == '__main__':
             reference_img_name = imageDownloader.download(output_dir, image_url)
             if reference_img_name is not None:  # case when the reference image is no longer available thus download fails
                 reference_img_path = os.path.join(output_dir, reference_img_name)
-                for img in candidate_imgs:
-                    img_path = os.path.join(output_dir, img)
-                    try:
-                        result = DeepFace.verify(reference_img_path, img_path, enforce_detection=False,
-                                                 model_name='VGG-Face')
-                    except AttributeError as e:# case when input image is a pdf, then the package can not read it.
-                        log.info(e)
-                        result = {"verified": False}
 
-                    if not result["verified"]:
-                        os.remove(img_path)
-                    else:
-                        valid_img += 1
+                if MULTIPROCESS:
+                    # Not working yet, stuck
+                    executor = ProcessPoolExecutor(max_workers=NUM_WORKERS)
+                    futures = [executor.submit(verify, reference_img_path, os.path.join(output_dir, img)) for
+                               img in candidate_imgs]
+                    for future in as_completed(futures):
+                        """wait for all process to finish"""
+                        valid_img += future.result()
+
+                else:
+                    for img in candidate_imgs:
+                        img_path = os.path.join(output_dir, img)
+                        result = verify(reference_img_path, img_path)
+                        valid_img += result
 
                 _, ext = os.path.splitext(reference_img_name)
                 os.rename(reference_img_path, os.path.join(output_dir, "reference_img" + ext))
@@ -71,6 +91,12 @@ if __name__ == '__main__':
         if valid_img == 0:
             shutil.rmtree(output_dir)
         log.info(f"valid images = {valid_img}")
+
+        # write to disk in order to resume if necessary
+        with open(query_fn, "w", newline='') as csvfile:
+            spamwriter = csv.writer(csvfile, delimiter=',')
+            for row in line_list:
+                spamwriter.writerow(row)
 
     time_identifier: str = get_time_identifier()
     s3Uploader = S3Uploader(path_identifier=time_identifier, dir="margin_project/KidFace")
