4076b3206d122f968f780844428f53117b0db0b4
Saibo-creator <saibo.geng@yahoo.fr>
2021-04-02 17:32:11 +0200

    [feat] use yaml to set experiment configurations

--- run_download_and_label.py
+++ run_download_and_label.py
@@ -2,9 +2,13 @@ import copy
 import shutil
 import os
 import time
+import yaml
+import argparse
 
 import csv
 import logging
+from os.path import abspath,dirname
+from imdb import IMDb
 from ImageLabelingPackage.ImageDownloadLabeler import ImageDownloader
 from deepface import DeepFace
 from PyS3Upload.PyS3Uploader import S3Uploader
@@ -26,37 +30,65 @@ def verify(reference_img_path, img_path):
     else:
         return 1
 
+
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-c", "--config", default="imdb")
+
+    args = parser.parse_args()
+    config_file = args.config
 
     log = logging.getLogger(__name__)
     log.setLevel(logging.INFO)
 
-    IMAGE_RECALL_NUM = 50
-    REMOVE_REFERENCE_IMG = False
-    REMOVE_LOCAL_AFTER_UPLOAD = True
-    MULTIPROCESS = False
-    NUM_WORKERS = 5
+    with open(dirname(abspath(__file__)) + f'/../configs/{config_file}.yaml') as f:
+
+        config = yaml.load(f, Loader=yaml.FullLoader)
+
+    IMAGE_RECALL_NUM = config["IMAGE_RECALL_NUM"]
+    REMOVE_REFERENCE_IMG = config["REMOVE_REFERENCE_IMG"]
+    REMOVE_LOCAL_AFTER_UPLOAD = config["REMOVE_LOCAL_AFTER_UPLOAD"]
+    MULTIPROCESS = config["MULTIPROCESS"]
+    NUM_WORKERS = config["NUM_WORKERS"]
+
+    ia = IMDb()
 
     startTime = int(round(time.time()))
 
     line_list = []
-    query_fn = 'query_results/has_img/query_2000_2020_6147_rest.csv'
-    # runtime_query_fn = f'{query_fn.split(".")[0]}_runtime.csv'
+    query_path = os.path.join(dirname(dirname(abspath(__file__))), config["query_path"])
+    query_path_, ext = os.path.splitext(query_path)
 
-    with open(query_fn, newline='') as csvfile:
+    runtime_query_path = query_path_+"_runtime"+ext
+    if not os.path.exists(runtime_query_path):
+        shutil.copy(query_path, runtime_query_path)
+
+    with open(runtime_query_path, newline='') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=',')
-        for row in spamreader:
-            line_list.append(row)
+        for i, row in enumerate(spamreader):
+            if i == 0:
+                col_names = row
+            else:
+                line_list.append(row)
 
     imageDownloader = ImageDownloader()
-    img_root_dir = 'img/google/kids_actor_11_no_safe'
+    col_names_dict = {col_name: index for index, col_name in enumerate(col_names)}
+    img_root_dir = config["img_root_dir"]
     while len(line_list) > 0:
         line = line_list.pop(0)
-        search_pair = (line[0], line[2], line[3])
-        folder_name = search_pair[0].replace(" ", "_")
-        name = search_pair[0]
-        birthdate = search_pair[1]
-        image_url = search_pair[2]
+        name = line[col_names_dict["itemLabel"]]
+        birthdate = line[col_names_dict["dateOfBirth"]]
+        if "image" in col_names_dict:
+            image_url = line[col_names_dict["image"]]
+        elif "IMDb_ID" in col_names_dict:
+            imdb_id = line[col_names_dict["IMDb_ID"]].replace("nm", "")
+            person = dict(ia.get_person(imdb_id).items())
+            image_url = person.get("full-size headshot")
+        else:
+            raise RuntimeError("No reference image url column available in the csv file")
+
+        folder_name = name.replace(" ", "_")
         output_dir = os.path.join(img_root_dir, folder_name)
         argv = ['-e', 'Google', '-d', 'chrome_headless', '-n', f'{IMAGE_RECALL_NUM}', '-j', '10', '-o',
                 output_dir, '-F', '-S', f'{name}', '-B', f"{birthdate}"]
@@ -65,12 +97,11 @@ if __name__ == '__main__':
         valid_img = 0
         if num_img > 0:
             candidate_imgs = os.listdir(output_dir)
-            reference_img_name = imageDownloader.download(output_dir, image_url)
+            reference_img_name = imageDownloader.download(output_dir, image_url, imagename="reference_img")
             if reference_img_name is not None:  # case when the reference image is no longer available thus download fails
                 reference_img_path = os.path.join(output_dir, reference_img_name)
 
                 if MULTIPROCESS:
-                    # Not working yet, stuck
                     executor = ProcessPoolExecutor(max_workers=NUM_WORKERS)
                     futures = [executor.submit(verify, reference_img_path, os.path.join(output_dir, img)) for
                                img in candidate_imgs]
@@ -93,13 +124,14 @@ if __name__ == '__main__':
         log.info(f"valid images = {valid_img}")
 
         # write to disk in order to resume if necessary
-        with open(query_fn, "w", newline='') as csvfile:
+        with open(runtime_query_path, "w", newline='') as csvfile:
             spamwriter = csv.writer(csvfile, delimiter=',')
+            spamwriter.writerow(col_names)
             for row in line_list:
                 spamwriter.writerow(row)
 
     time_identifier: str = get_time_identifier()
-    s3Uploader = S3Uploader(path_identifier=time_identifier, dir="margin_project/KidFace")
+    s3Uploader = S3Uploader(path_identifier=time_identifier, dir=config["s3_dir"])
 
     filepath = shutil.make_archive(base_name="images", format='tar', root_dir=img_root_dir)
 
